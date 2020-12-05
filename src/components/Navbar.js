e17930464866d75c91238f277deba87d6547a119
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-12-05 02:32:06 -0500

    added navbar styling and link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import {AppBar, Toolbar, Typography} from '@material-ui/core'
+import {AppBar, Toolbar} from '@material-ui/core'
+import {Link} from 'gatsby'
 
 const Navbar = () => {
     return(
         <AppBar position='static'>
             <Toolbar>
                 <img src={'/assets/favicon.svg'} />
+                <ul className='nav-options'>
+                    <li className='nav-option'>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li className='nav-option'>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
             </Toolbar>
         </AppBar>
     )
