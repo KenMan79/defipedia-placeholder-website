bbc0349a363b3ec5c30d06a57b7e2c9e424ac6a7
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-12-05 23:48:34 -0500

    cotact page layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Layout from '../components/layout';
+import {Paper, TextField, Button} from '@material-ui/core'
 
 const ContactPage = () => {
     return(
-        <div className='contact-page'>
-            <p>This is conact page</p>
-        </div>
+        <Layout>
+            <Paper className='contact-form-container' elevation={3}>
+                <h2>Get in Touch</h2>
+                <form>
+                    <TextField label='Full Name' variant='outlined' />
+                    <TextField label='Email' variant='outlined' />
+                    <textarea placeholder='Enter your message....'/>
+                    <Button variant="contained" color="primary">
+                        Send
+                    </Button>
+                    <hr />
+                    <section className='social-media-handle'>
+                        <i class="fab fa-discord"></i>
+                        <i class="fab fa-twitter-square"></i>
+                        <i class="fab fa-linkedin"></i>
+                        <i class="fab fa-github-square"></i>
+                        <i class="fab fa-telegram"></i>
+                        <i class="fab fa-medium"></i>
+                    </section>
+                </form>
+            </Paper>
+        </Layout>
     )
 }
 
