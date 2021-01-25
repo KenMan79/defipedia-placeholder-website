2263bf24aa15a64f9fc44dabf93b94c920921389
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-24 23:47:04 -0500

    styling fix and base layout setup

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,17 +2,20 @@ import React from'react'
 import Layout from '../components/layout';
 import TeamCard from '../components/TeamCard';
 import team from '../data/team.json';
-
+import{Grid} from '@material-ui/core';
 
 const AboutPage = () => {
     return(
         <Layout>
             <div className='about-page'>
                 <h1>Our Team</h1>
-                {team.map(member => (
-                    // console.log(member)
-                    <TeamCard name={member.name} picture='' role={member.role}/>
-                ))}
+                <Grid container >
+                    {team.map(member => (
+                        <Grid item xs={12} sm={12} md={4} align='center'>
+                            <TeamCard name={member.name} picture='' role={member.role}/>
+                        </Grid>
+                    ))}
+                </Grid>
             </div>
         </Layout>
     )
